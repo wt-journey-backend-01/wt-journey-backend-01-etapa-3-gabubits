a43edb035f568cbdcdab5e179fca925a7ba900dc
Gabriel <gomesg827@gmail.com>
2025-08-07 18:50:35 -0300

    Seeds separadas entre agentes e casos.

--- db/seeds/casos.js
+++ db/seeds/casos.js
@@ -3,21 +3,7 @@
  * @returns { Promise<void> }
  */
 export async function seed(knex) {
-  await knex("agentes").del();
   await knex("casos").del();
-
-  await knex("agentes").insert([
-    {
-      nome: "Rommel Carneiro",
-      dataDeIncorporacao: "1992-10-04",
-      cargo: "delegado",
-    },
-    {
-      nome: "Luciana Farias",
-      dataDeIncorporacao: "2005-06-17",
-      cargo: "inspetor",
-    },
-  ]);
 
   await knex("casos").insert([
     {
